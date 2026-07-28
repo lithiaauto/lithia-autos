188be4eedc08c7f3318be7932008411d8f8bf632
lithiaauto <lithiaautos4@gmail.com>
2026-07-28 04:35:14 -0400

    Replace Smartsupp live chat with Tawk.to

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -159,23 +159,23 @@ export default function RootLayout({
 
         <CookieConsent />
 
-        {/* Smartsupp Live Chat */}
+        {/* Tawk.to Live Chat */}
         <script
           type="text/javascript"
           dangerouslySetInnerHTML={{
             __html: `
-              var _smartsupp = _smartsupp || {};
-              _smartsupp.key = '68994566b65e974b51ab7e4d789412964959c3b8';
-              window.smartsupp||(function(d) {
-                var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
-                s=d.getElementsByTagName('script')[0];c=d.createElement('script');
-                c.type='text/javascript';c.charset='utf-8';c.async=true;
-                c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
-              })(document);
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/6a685ac63e5c1e1d43355949/1jujq56j2';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+              })();
             `
           }}
         />
-        <noscript>Powered by <a href="https://www.smartsupp.com" target="_blank" rel="noopener noreferrer">Smartsupp</a></noscript>
       </body>
     </html>
   );
